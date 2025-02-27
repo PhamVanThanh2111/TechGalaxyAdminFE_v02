6db0afa05464d13b120af9f793ab94d9014cd5fe
PhamVanThanh <phamtrung21112003@gmail.com>
2025-02-27 09:43:33 +0700

    first commit

---
+++ src/main/java/iuh/fit/se/techgalaxy/frontend/admin/techgalaxyadminfe/TechGalaxyAdminFeApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.techgalaxy.frontend.admin.techgalaxyadminfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechGalaxyAdminFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechGalaxyAdminFeApplication.class, args);
+	}
+
+}
