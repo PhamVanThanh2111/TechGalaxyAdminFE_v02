3474dceefe8bddebcf0cf6749c6925a82d63b6d7
Phạm Văn Thành <21089891.thanh@student.iuh.edu.vn>
2025-04-24 23:44:15 +0700

    update timeout

--- src/main/java/iuh/fit/se/techgalaxy/frontend/admin/techgalaxyadminfe/configs/RestClientConfig.java
+++ src/main/java/iuh/fit/se/techgalaxy/frontend/admin/techgalaxyadminfe/configs/RestClientConfig.java
@@ -1,13 +1,9 @@
 package iuh.fit.se.techgalaxy.frontend.admin.techgalaxyadminfe.configs;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class RestClientConfig {
@@ -15,8 +11,8 @@ public class RestClientConfig {
     @Bean
     public RestClient restClient() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(500);
-        factory.setReadTimeout(500);
+        factory.setConnectTimeout(1500);
+        factory.setReadTimeout(1500);
 
         return RestClient.builder()
                 .requestFactory(factory)
